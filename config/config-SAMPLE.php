f31aeaae706ac4f9aa61682d84fbaa9100854429
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2015-10-22 16:38:56 -0200

    Config file and shell script to install dependences

---
+++ config/config-SAMPLE.php
@@ -0,0 +1,26 @@
+<?php
+
+default_lang = 'pt'
+items_per_page = 10
+
+[EBSCO]
+
+api_url = ''
+profile = ''
+password = ''
+
+[MICROMEDEX]
+
+deeplink_url = ''
+institution = ''
+
+[ACCESSMEDICINE]
+
+search_url = ''
+
+[BVS]
+
+api_url = ''
+
+
+?>
