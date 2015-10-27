eb09c5f2fb7fac1dc28becf3956e0630795b8d86
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2015-10-27 12:03:23 -0200

    Implement pagination

--- views/search_bvs.php
+++ views/search_bvs.php
@@ -9,22 +9,48 @@ $app->match('search_bvs/', function (Request $request) use ($app, $config) {
         $app['request']->query->all()
     );
 
+    $request_uri = preg_replace('/&page=(\d+)/','', $_SERVER['REQUEST_URI']);
+
     $db_config = $config['BVS'];
     $service_url = $db_config['api_url'];
     $query = $params['q'];
 
+    if ($db_config['items_per_page'] != ''){
+        $count = $db_config['items_per_page'];
+    }else{
+        $count = $config['items_per_page'];
+    }
+
     $request_url = $service_url . '&q=' . $query . '&count=' . $config['items_per_page'];
 
-    $result_url = $db_config['result_url'] . '&q=' . $query . '&count=' . $config['items_per_page'];
+    $result_url = $db_config['result_url'] . '&q=' . $query . '&count=' . $count;
+
+    if ($params['page'] > 1){
+        $from = ($params['page'] * $count) + 1;
+        $request_url.= '&from=' . $from;
+    }
 
     $result_json = @file_get_contents($request_url);
 
     if ($result_json){
         $result = json_decode($result_json, true);
+        $total_hits = $result['diaServerResponse'][0]['response']['numFound'];
     }
 
+    if ($total_hits > 0){
+        $pagination['page'] = ($params['page'] > 0 ? $params['page'] : 1);
+        $pagination['num_pages'] = (int)($total_hits/$count);
+        $pagination['has_next'] = ($pagination['page'] >= $pagination['num_pages']? false : true);
+        $pagination['has_previous'] = ($pagination['page'] == 1? false : true);
+    }
+
+    $output['total_hits'] = $total_hits;
     $output['item_list'] = $result['diaServerResponse'][0]['response']['docs'];
     $output['result_url'] = $result_url;
+    $output['pagination'] = $pagination;
+    $output['request_uri'] = $request_uri;
+    $output['box'] = $params['box'];
+
 
     return $app['twig']->render('bvs.html', $output);
 
--- views/search_ebsco_api.php
+++ views/search_ebsco_api.php
@@ -9,22 +9,48 @@ $app->match('search_ebsco/', function (Request $request) use ($app, $config) {
             $app['request']->query->all()
     );
 
+    $request_uri = preg_replace('/&page=(\d+)/','', $_SERVER['REQUEST_URI']);
+
     $db_config = $config['EBSCO'];
+
+    if ($db_config['items_per_page'] != ''){
+        $count = $db_config['items_per_page'];
+    }else{
+        $count = $config['items_per_page'];
+    }
+
     $service_url = $db_config['api_url'] . 'Search?prof=' . $db_config['profile'] . '&pwd=' . $db_config['password'] . '&authType=' . $db_config['auth_type'];
-    $service_url.= '&format=brief&num_rec=' . $config['items_per_page'];
+    $service_url.= '&format=brief&num_rec=' . $count;
+
+    if ($params['page'] > 1){
+        $from = ($params['page'] * $count) + 1;
+        $service_url.= '&startrec=' . $from;
+    }
 
     $query = $params['q'];
     $db = $params['db'];
 
     $request_url = $service_url . '&query=' . $query . '&db=' . $db;
-    
+
     $service_xml = @simplexml_load_file($request_url);
 
     if ($service_xml){
         $item_list = $service_xml->SearchResults->records->rec;
+        $total_hits = (int) $service_xml->Hits;
     }
 
+    if ($total_hits > 0){
+        $pagination['page'] = ($params['page'] > 0 ? $params['page'] : 1);
+        $pagination['num_pages'] = (int)($total_hits/$count);
+        $pagination['has_next'] = ($pagination['page'] >= $pagination['num_pages']? false : true);
+        $pagination['has_previous'] = ($pagination['page'] == 1? false : true);
+    }
+
+    $output['total_hits'] = $total_hits;
     $output['item_list'] = $item_list;
+    $output['pagination'] = $pagination;
+    $output['box'] = $params['box'];
+    $output['request_uri'] = $request_uri;
 
     return $app['twig']->render('ebsco.html', $output);
 
