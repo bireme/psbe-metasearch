752c98da575904db5933cb2a2a7a67acc6200f01
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2016-09-05 16:16:32 -0300

    Adiciona novas fontes no config de exemplo

--- config/config-SAMPLE.php
+++ config/config-SAMPLE.php
@@ -30,4 +30,23 @@ filter_list = ''
 items_per_page = 20
 
 
+[REBRATS]
+
+search_url = 'http://189.28.128.101/rebrats/visao/sociedade/estudo.cfm'
+search_params = 'novaPesquisa=true&evento=5&frmEstudoValida=5&st_consulta=OR&nu_ano_publicacao=&co_seq_tipo_estudo=&tp_estudo=T&tp_estudo_avaliado=&envia=0'
+
+[EPISTEMONIKOS]
+
+search_url = 'http://www.epistemonikos.org/pt/search'
+
+[BULARIO]
+
+result_url = 'http://www.anvisa.gov.br/datavisa/fila_bula/frmResultado.asp'
+resul_params = 'txtPageSize=10'
+
+[PROQUALIS]
+
+search_url = 'http://proqualis.net/search/site/'
+
+
 ?>
